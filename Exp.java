fd464e8cbab27b625b20628589fe82d9784f57e7
99s <divi9e@gmail.com>
2016-02-20 16:59:36 +0530

    Added files via upload

---
+++ Exp.java
@@ -0,0 +1,24 @@
+/*Assume that the integer variable x is 5 and the integer variable y is 10. Give the values of the following
+expressions:
+a. x + y * 2
+b. x - y + 2
+c. (x+y) * 2
+d. y % x
+*/
+import java.io.DataInputStream;
+class Exp
+{
+	public static void main(String [] args)
+	{
+		int x = 5,y = 10;
+		int a = x + y * 2;
+		int b = x - y + 2;
+		int c = (x+y) * 2;
+		int d = y % x;
+		System.out.println (" x = 5 , y = 10");
+		System.out.println ("x + y * 2 = " + a);
+		System.out.println ("x - y + 2 = " + b);
+		System.out.println ("(x+y) * 2 = " + c);
+		System.out.println ("y % x = " + d);
+	}
+}
